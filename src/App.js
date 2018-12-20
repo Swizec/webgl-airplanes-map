c87fddefb8a0a74bbee13aa26cd16307101e052c
Swizec Teller <swizec@swizec.com>
2018-12-20 16:46:45 +0100

    Constantly moving airplanes with interploates?

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import DeckGL, { IconLayer } from "deck.gl";
 import { StaticMap } from "react-map-gl";
 import * as d3 from "d3";
+
 import Airplane from "./airplane-icon.jpg";
+import destinationPoint from "./destinationPoint";
 
 // Set your mapbox access token here
 const MAPBOX_ACCESS_TOKEN =
@@ -20,30 +21,76 @@ const initialViewState = {
 };
 
 class App extends Component {
-    state = { airplanes: [] };
+    state = {
+        airplanes: []
+    };
+    currentFrame = null;
+    timer = null;
+    fetchEverySeconds = 10;
+    framesPerFetch = this.fetchEverySeconds * 60; // 60fps, 10 second intervals
 
     componentDidMount() {
         this.fetchData();
     }
 
     fetchData = () => {
-        console.log("fetching data");
         d3.json("https://opensky-network.org/api/states/all").then(
             ({ states }) =>
-                this.setState({
-                    // from https://opensky-network.org/apidoc/rest.html#response
-                    airplanes: states.map(d => ({
-                        callsign: d[1],
-                        longitude: d[5],
-                        latitude: d[6],
-                        velocity: d[9],
-                        altitude: d[13],
-                        origin_country: d[2],
-                        true_track: d[10]
-                    }))
-                })
+                this.setState(
+                    {
+                        // from https://opensky-network.org/apidoc/rest.html#response
+                        airplanes: states.map(d => ({
+                            callsign: d[1],
+                            longitude: d[5],
+                            latitude: d[6],
+                            velocity: d[9],
+                            altitude: d[13],
+                            origin_country: d[2],
+                            true_track: d[10],
+                            interpolatePos: d3.geoInterpolate(
+                                [d[5], d[6]],
+                                destinationPoint(
+                                    d[5],
+                                    d[6],
+                                    d[9] * this.fetchEverySeconds,
+                                    (d[10] * 180) / Math.PI
+                                )
+                            )
+                        }))
+                    },
+                    () => {
+                        this.startAnimation();
+                        setTimeout(
+                            this.fetchData,
+                            this.fetchEverySeconds * 1000
+                        );
+                    }
+                )
         );
-        setTimeout(this.fetchData, 10 * 1000);
+    };
+
+    startAnimation = () => {
+        if (this.timer) {
+            this.timer.stop();
+        }
+        this.currentFrame = 0;
+        this.timer = d3.timer(this.animationFrame);
+    };
+
+    animationFrame = () => {
+        let { airplanes } = this.state;
+        airplanes = airplanes.map(d => {
+            const [longitude, latitude] = d.interpolatePos(
+                this.currentFrame / this.framesPerFetch
+            );
+            return {
+                ...d,
+                longitude,
+                latitude
+            };
+        });
+        this.currentFrame += 1;
+        this.setState({ airplanes });
     };
 
     render() {
@@ -64,7 +111,7 @@ class App extends Component {
                 sizeScale: 20,
                 getPosition: d => [d.longitude, d.latitude],
                 getIcon: d => "airplane",
-                getAngle: d => 45 + (d.true_track * 180) / Math.PI
+                getAngle: d => 45 + (-d.true_track * 180) / Math.PI
             })
         ];
 
